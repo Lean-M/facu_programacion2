c80b9ed7312e0e8a183a4ab14914388612764030
Leandro <joaquinleandromuro@gmail.com>
2018-09-12 13:37:44 -0300

    me olvide de guardar lo del zoo -.-

--- src/tp5/Zoologico/Animal.java
+++ src/tp5/Zoologico/Animal.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tp5.Zoologico;
 
-/**
- *
- * @author lean
- */
+import java.util.Map;
+
 public class Animal {
+    private String nombre;
+    private Map atributo;
     
 }
